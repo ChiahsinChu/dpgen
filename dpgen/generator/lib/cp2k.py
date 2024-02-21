0355e77a5f963473e98c0087c2d53bb087764e39
Jia-Xin Zhu <jiaxinzhu@stu.xmu.edu.cn>
2024-02-21 10:00:25 +0800

    fix bug in cp2k iterdict

--- dpgen/generator/lib/cp2k.py
+++ dpgen/generator/lib/cp2k.py
@@ -19,11 +19,61 @@ default_config = {
         "SUBSYS": {
             "CELL": {"A": "10 .0 .0", "B": ".0 10 .0", "C": ".0 .0 10"},
             "COORD": {"@include": "coord.xyz"},
-            "KIND": {
-                "_": ["H", "C", "N"],
-                "POTENTIAL": ["GTH-PBE-q1", "GTH-PBE-q4", "GTH-PBE-q5"],
-                "BASIS_SET": ["DZVP-MOLOPT-GTH", "DZVP-MOLOPT-GTH", "DZVP-MOLOPT-GTH"],
-            },
+            "KIND": [
+                {
+                    "_": "O",
+                    "POTENTIAL": "GTH-PBE-q6",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "H",
+                    "POTENTIAL": "GTH-PBE-q1",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "Pt",
+                    "POTENTIAL": "GTH-PBE-q10",
+                    "BASIS_SET": "DZVP-A5-Q10-323-MOL-T1-DERIVED_SET-1"
+                }, {
+                    "_": "Ag",
+                    "POTENTIAL": "GTH-PBE-q11",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "Na",
+                    "POTENTIAL": "GTH-PBE-q9",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "K",
+                    "POTENTIAL": "GTH-PBE-q9",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "Li",
+                    "POTENTIAL": "GTH-PBE-q3",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "C",
+                    "POTENTIAL": "GTH-PBE-q4",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "N",
+                    "POTENTIAL": "GTH-PBE-q5",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "Cl",
+                    "POTENTIAL": "GTH-PBE-q7",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "F",
+                    "POTENTIAL": "GTH-PBE-q7",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "Mg",
+                    "POTENTIAL": "GTH-PBE-q10",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }, {
+                    "_": "Al",
+                    "POTENTIAL": "GTH-PBE-q3",
+                    "BASIS_SET": "DZVP-MOLOPT-SR-GTH"
+                }
+            ]
         },
         "PRINT": {"FORCES": {"_": "ON"}, "STRESS_TENSOR": {"_": "ON"}},
     },
@@ -48,64 +98,47 @@ def update_dict(old_d, update_d):
             old_d[k] = update_d[k]
 
 
-def iterdict(d, out_list, flag=None):
-    """:doc: a recursive expansion of dictionary into cp2k input
-    :k: current key
-    :v: current value
-    :d: current dictionary under expansion
-    :flag: used to record dictionary state. if flag is None,
-    it means we are in top level dict. flag is a string.
+def iterdict(input_dict, out_list, loop_idx):
+    """ 
+    recursively generate a list of strings for further 
+    print out CP2K input file
+
+    Args:
+        input_dict: dictionary for CP2K input parameters
+        out_list: list of strings for printing
+        loop_idx: record of loop levels in recursion
+    Return:
+        out_list
     """
-    for k, v in d.items():
+    if len(out_list) == 0:
+        out_list.append("\n")
+    start_idx = len(out_list) - loop_idx - 2
+    for k, v in input_dict.items():
         k = str(k)  # cast key into string
-        # if value is dictionary
+        #if value is dictionary
         if isinstance(v, dict):
-            # flag == None, it is now in top level section of cp2k
-            if flag is None:
-                out_list.append("&" + k)
-                out_list.append("&END " + k)
-                iterdict(v, out_list, k)
-            # flag is not None, now it has name of section
-            else:
-                index = out_list.index("&END " + flag)
-                out_list.insert(index, "&" + k)
-                out_list.insert(index + 1, "&END " + k)
-                iterdict(v, out_list, k)
+            out_list.insert(-1 - loop_idx, "&" + k)
+            out_list.insert(-1 - loop_idx, "&END " + k)
+            iterdict(v, out_list, loop_idx + 1)
+        # if value is list
         elif isinstance(v, list):
-            #            print("we have encountered the repeat section!")
-            index = out_list.index("&" + flag)
-            # delete the current constructed repeat section
-            del out_list[index : index + 2]
-            # do a loop over key and corresponding list
-            k_tmp_list = []
-            v_list_tmp_list = []
-            for k_tmp, v_tmp in d.items():
-                k_tmp_list.append(str(k_tmp))
-                v_list_tmp_list.append(v_tmp)
-            for repeat_keyword in zip(*v_list_tmp_list):
-                out_list.insert(index, "&" + flag)
-                out_list.insert(index + 1, "&END " + flag)
-                for idx, k_tmp in enumerate(k_tmp_list):
-                    if k_tmp == "_":
-                        out_list[index] = "&" + flag + " " + repeat_keyword[idx]
-                    else:
-                        out_list.insert(index + 1, k_tmp + " " + repeat_keyword[idx])
-
-            break
-
+            if isinstance(v[0], dict):
+                for _v in v:
+                    out_list.insert(-1 - loop_idx, "&" + k)
+                    out_list.insert(-1 - loop_idx, "&END " + k)
+                    iterdict(_v, out_list, loop_idx + 1)
+            else:
+                for _v in v:
+                    _v = str(_v)
+                    out_list.insert(-1 - loop_idx, k + " " + _v)
+        # if value is other type, e.g., int/float/str
         else:
             v = str(v)
-            if flag is None:
-                out_list.append(k + " " + v)
-                print(k, ":", v)
+            if k == "_":
+                out_list[start_idx] = out_list[start_idx] + " " + v
             else:
-                if k == "_":
-                    index = out_list.index("&" + flag)
-                    out_list[index] = "&" + flag + " " + v
-
-                else:
-                    index = out_list.index("&END " + flag)
-                    out_list.insert(index, k + " " + v)
+                out_list.insert(-1 - loop_idx, k + " " + v)
+    return out_list
 
 
 def make_cp2k_input(sys_data, fp_params):
@@ -128,9 +161,9 @@ def make_cp2k_input(sys_data, fp_params):
     update_dict(default_config, user_config)
     update_dict(default_config, cell_config)
     # output list
-    input_str = []
-    iterdict(default_config, input_str)
+    input_str = iterdict(default_config, out_list=["\n"], loop_idx=0)
     string = "\n".join(input_str)
+    string = string.strip("\n")
     return string
 
 
